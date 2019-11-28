3f6f82d3bea91ca6577f61b3efa182137ac83e80
Jeroen van Kessel <krabbedoelie@hotmail.com>
2019-11-28 16:02:31 +0100

    Create backup-vultr-snapshot.sh

---
+++ backup-vultr-snapshot.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#By krabelize | cryptsus.com
+#Create automatic snapshots on vultr.com and delete older snapshots
+#Go to https://my.vultr.com/settings/#settingsapi to get your API key
+#Only allow the Vultr API to be accessed from your bastion /32 Vultr instance(s)
+#Install jquery on your bastion VPS (pkg_add jq)
+#Add this script to your crontab under a non-root account
+#crontab -e
+#0 6 * * * /home/username/backup-vultr-snapshot.sh
+
+#Variables
+#Put your API key here
+api_key="your_vultr_api_key_here"
+#We keep the total number of snapshots under 10 based on two virtual private servers (VPS)
+snapshot_limit="8"
+
+#Query all VPS SUBIDs
+VPS_names=$(curl -s "https://api.vultr.com/v1/server/list?api_key=$api_key" | jq -r 'keys | '.[]'')
+#Query all snapshots and count them
+snapshot_count=$(curl -s "https://api.vultr.com/v1/snapshot/list?api_key=$api_key" | jq -r 'keys | .[]' | wc -l)
+#Query oldest snapshot created
+last_snapshot_ID=$(curl -s "https://api.vultr.com/v1/snapshot/list?api_key=$api_key" | jq -r 'keys | .[]' | tail -1)
+
+#Deleting the oldest snapshot present with Snapshot ID
+if [ "$snapshot_count" -gt "$snapshot_limit" ]; then
+    curl -s "https://api.vultr.com/v1/snapshot/destroy?api_key=$api_key" --data SNAPSHOTID=$last_snapshot_ID
+    if [ "$?" -eq "0" ]; then
+        logger "[Vultr.com] Deleted Snapshot ID: '$last_snapshot_ID'"
+    else
+        logger "[Vultr.com] Failed to delete snapshot ID: '$last_snapshot_ID'"
+    fi
+fi
+
+for vps in $VPS_names; do
+    VPS_label=$(curl -s "https://api.vultr.com/v1/server/list?api_key=$api_key&SUBID=$vps" | jq -r '.label')
+    if curl -s "https://api.vultr.com/v1/snapshot/create?api_key=$api_key" --data SUBID=$vps --data description=$VPS_label | grep -q 'SNAPSHOTID'; then
+        logger "[OK - Backup] Creating a snapshot for VPS on Vultr: '$VPS_label' with SUBID: '$vps'"
+    else
+        logger "[FAILED - Backup] Failed to create snapshot for VPS on Vultr: '$VPS_label' with SUBID: '$vps'"
+    fi
+done

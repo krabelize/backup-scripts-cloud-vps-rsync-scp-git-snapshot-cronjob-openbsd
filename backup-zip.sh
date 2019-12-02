1d944d45a53034c5c2965950ba93673c21cc069f
Jeroen van Kessel <krabbedoelie@hotmail.com>
2019-12-02 16:28:16 +0100

    Update backup-zip.sh

--- backup-zip.sh
+++ backup-zip.sh
@@ -1,33 +1,41 @@
 #!/bin/sh
 #By krabelize | cryptsus.com
-#httpd htdocs compressed zip directory backup script tested on OpenBSD 6.6
-
-#Edit crontab with the following entry
-#Run this script every night at 05:00
+#httpd htdocs compressed zip and OpenSSL encrypted backup script. Tested on OpenBSD 6.6
+#First create an EAS256 key. See blog post for info
+#Edit crontab with the following entry. Run this script every night at 05:00
 #crontab -e
 #0 4 * * * /home/$username/backup-zip.sh
 
 #Variables
 source="/var/www/htdocs"
 username="username"
-backup_name="backup_webdir_webname"
-machine="$username@10.0.0.2"
-destination="/home/$username/backup-zip"
+dir_name="backup_webdir_cryptsus"
 backup_date=$(date +"%d-%m-%Y")
+backup_name="/home/$username/$dir_name-$backup_date.zip"
+machine="$username@10.0.0.2"
+remote_destination="/home/$username/backup-zip"
+aes_key="/home/$username/backup.key"
 port="22"
 
-zip -r /home/"$username"/"$backup_name"-"$backup_date".zip $source
+zip -r $backup_name $source
 if [ $? -eq 0 ]; then
-    logger "zip backup is successfully created"
-    scp -P $port /home/"$username"/"$backup_name"-"$backup_date".zip $machine:$destination
+    echo "zip backup file is successfully created"
+    openssl enc -in $backup_name -out $backup_name.enc -pass file:$aes_key -e -salt -aes-256-cbc -pbkdf2
+    if [ $? -eq 0 ]; then
+        echo "encrypted zip backup file is successfully created"
+        scp -P $port $backup_name.enc $machine:$remote_destination
         if [ $? -eq 0 ]; then
-            logger "[OK - Backup] ssh backup transfer is successful"
+            echo "[OK - Backup] ssh encrypted zip backup file transfer is successful"
             exit 0
         else
-            logger "[FAILED - Backup] ssh backup transfer failed. Please check your backup script or system settings"
+            echo "[FAILED - Backup] ssh encrypted zip transfer failed. Please check your backup script or system settings"
             exit 1
         fi
+    else
+        echo "encrypted zip backup file failed to created"
+        exit 1
+    fi
 else
-    logger "[FAILED - Backup] zip backup file was not created. Please check your backup script or system settings"
+    echo "[FAILED - Backup] zip backup file was not created. Please check your backup script or system settings"
     exit 1
 fi

cccb49912c100c8b891a4321b772567602500677
Jeroen van Kessel <krabbedoelie@hotmail.com>
2019-12-04 14:38:36 +0100

    Update backup-zip.sh

--- backup-zip.sh
+++ backup-zip.sh
@@ -19,23 +19,23 @@ port="22"
 
 zip -r $backup_name $source
 if [ $? -eq 0 ]; then
-    echo "zip backup file is successfully created"
+    #logger "zip backup file is successfully created"
     openssl enc -in $backup_name -out $backup_name.enc -pass file:$aes_key -e -salt -aes-256-cbc -pbkdf2
     if [ $? -eq 0 ]; then
-        echo "encrypted zip backup file is successfully created"
+        #logger "encrypted zip backup file is successfully created"
         scp -P $port $backup_name.enc $machine:$remote_destination
         if [ $? -eq 0 ]; then
-            echo "[OK - Backup] ssh encrypted zip backup file transfer is successful"
+            logger "[OK - Backup] ssh encrypted zip backup file transfer is successful"
             exit 0
         else
-            echo "[FAILED - Backup] ssh encrypted zip transfer failed. Please check your backup script or system settings"
+            logger "[FAILED - Backup] ssh encrypted zip transfer failed. Please check your backup script or system settings"
             exit 1
         fi
     else
-        echo "encrypted zip backup file failed to created"
+        logger "[FAILED - Backup] encrypted zip backup file failed to created"
         exit 1
     fi
 else
-    echo "[FAILED - Backup] zip backup file was not created. Please check your backup script or system settings"
+    logger "[FAILED - Backup] zip backup file was not created. Please check your backup script or system settings"
     exit 1
 fi
